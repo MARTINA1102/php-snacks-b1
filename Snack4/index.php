061bb6395573e6cdc5602a70efec86b8a861a921
MARTINA1102 <martinamiccoli11@gmail.com>
2022-12-15 11:22:09 +0100

    corretto e finito

--- Snack4/index.php
+++ Snack4/index.php
@@ -1,11 +1,12 @@
 <?php
     $number = [];
     while (count($number) < 15) {
-        $numrandom.= rand(0,100);
+        $numrandom= rand(50,100);
         if (!in_array($numrandom,$number)) {
             $number[] = $numrandom;
         };
     };
+    var_dump($number);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,11 +14,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack4</title>
 </head>
 <body>
-    <div>
-        <ul><?php echo($numrandom)?></ul>
-    </div>
+
 </body>
 </html>
